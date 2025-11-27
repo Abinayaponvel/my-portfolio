be9331ecbaf4d56a9e23f820299ed7d2a0858b20
Abinayaponvel <abinayaponvel16@gmail.com>
2025-11-27 18:43:24 +0530

    Save local changes before rebase

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom"; // import BrowserRouter
 import App from "./App";
 import "./App.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename="/my-portfolio">
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
