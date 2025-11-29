0382c743c058f28aa6060c80ad69204c6e68fa4f
Abinayaponvel <144317461+Abinayaponvel@users.noreply.github.com>
2025-11-29 12:46:55 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
+// src/index.js (CORRECTED CODE)
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom"; // import BrowserRouter
+import { BrowserRouter } from "react-router-dom"; 
 import App from "./App";
 import "./App.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/my-portfolio">
+    <BrowserRouter> {/* <--- REMOVED: basename="/my-portfolio" */}
       <App />
     </BrowserRouter>
   </React.StrictMode>
